36ccee497d95aee23789bbab58c47a22c187540c
stewicca <stewicalf@gmail.com>
2025-06-19 13:03:07 +0700

    add: background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
-<script lang="ts">
-	import '../app.css';
-	
-	let { children } = $props();
+<script lang='ts'>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
+    import { Gradient } from '$lib/gradient';
+    import '../app.css';
+
+    let { children } = $props();
+
+    const gradient = new Gradient();
+
+    onMount(() => {
+        if (browser) {
+            gradient.initGradient('#gradient-canvas');
+        }
+    });
 </script>
 
 {@render children()}
